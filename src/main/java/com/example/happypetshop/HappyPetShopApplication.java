e0608196a2aa267822a98d4c2788623ec13b06e8
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-06-27 23:52:28 +0300

    started working on the HTML and CSS of the app

---
+++ src/main/java/com/example/happypetshop/HappyPetShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.happypetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyPetShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyPetShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/happypetshop/HappyPetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.happypetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyPetShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
