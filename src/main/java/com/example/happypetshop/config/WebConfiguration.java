1ae934a3660a1ceea37b38a3184a9f8822d099cc
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-08-12 17:54:56 +0300

    added MaintenanceInterceptor

---
+++ src/main/java/com/example/happypetshop/config/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.happypetshop.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final MaintenanceInterceptor maintenanceInterceptor;
+
+
+    public WebConfiguration(MaintenanceInterceptor maintenanceInterceptor) {
+        this.maintenanceInterceptor = maintenanceInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(maintenanceInterceptor);
+    }
+}
