e8355e9ed75a284311b386a01f19fd69144fbe81
Todor Todorov <78209454+TodorTodoroff@users.noreply.github.com>
2022-08-12 19:02:37 +0300

    implemented locale change interceptor

--- src/main/java/com/example/happypetshop/config/WebConfiguration.java
+++ src/main/java/com/example/happypetshop/config/WebConfiguration.java
@@ -4,19 +4,24 @@ package com.example.happypetshop.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final MaintenanceInterceptor maintenanceInterceptor;
 
+    private final LocaleChangeInterceptor localeChangeInterceptor;
 
-    public WebConfiguration(MaintenanceInterceptor maintenanceInterceptor) {
+
+    public WebConfiguration(MaintenanceInterceptor maintenanceInterceptor, LocaleChangeInterceptor localeChangeInterceptor) {
         this.maintenanceInterceptor = maintenanceInterceptor;
+        this.localeChangeInterceptor = localeChangeInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(maintenanceInterceptor);
+        registry.addInterceptor(localeChangeInterceptor);
     }
 }
